0b66e5c1543af1dcc279b5d24859997c3033861a
Michael Ruvinshteyn <mruvinshteyn@stuy.edu>
2017-04-01 19:57:16 -0400

    Added default constructor

--- Dequeue.java
+++ Dequeue.java
@@ -2,5 +2,11 @@ public class Dequeue implements Deque{
 
     private ArrayList<T> _queue;
     private int _size;
+    private int _capacity;
 
+    public Dequeue(int capacity){
+	_queue = new ArrayList<T>();
+	_size = 0;
+	_capacity = capacity;
+    }
 }
