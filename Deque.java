fbcee11e92efcb3d6be3a44a73ec65d661f10a25
Michael Ruvinshteyn <mruvinshteyn@stuy.edu>
2017-04-02 00:43:45 -0400

    Added methods to Deque

--- Deque.java
+++ Deque.java
@@ -2,11 +2,17 @@ public interface Deque<T>{
 
     public T poll();
 
+    public T pollLast();
+
     public T peek();
+
+    public T peekLast();
 
     public int size();
 
     public boolean offer(T x);
+
+    public boolean offerFirst(T x);
 
     public boolean contains(Object x);
 
